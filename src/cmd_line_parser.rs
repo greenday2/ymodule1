6c70ded0821d782101a7e15a936787c727d2e23c
Michael Gulev <greenday2.gd@gmail.com>
2026-07-10 12:52:03 +0700

    Simplify command line parser

--- src/cmd_line_parser.rs
+++ src/cmd_line_parser.rs
@@ -1,118 +1,37 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::env;
 
-struct CmdLineArgs {
-    back: Vec<String>,
-    original: std::env::Args,
-}
+pub type ArgsMap = HashMap<String, Option<String>>;
 
-impl CmdLineArgs {
-    pub fn new() -> Self {
-        CmdLineArgs {
-            original: env::args(),
-            back: Vec::with_capacity(2),
-        }
-    }
+pub fn parse_command_line_args() -> Result<ArgsMap, String> {
+    let possible_args = HashSet::<&str>::from_iter(["--help", "--in", "--out", "--dest-format"]);
+    let mut collected_args: ArgsMap = HashMap::new();
 
-    pub fn push_back(&mut self, arg: String) {
-        self.back.push(arg);
-    }
-
-    fn next(&mut self) -> Option<String> {
-        if let Some(arg) = self.back.pop() {
-            Some(arg)
-        } else {
-            self.original.next()
-        }
-    }
-}
-
-enum CmdLineArgValueKind {
-    Required,
-    NotRequired,
-}
-
-pub enum CmdLineParserResult {
-    NoArgs,
-    HelpSpecified,
-    ParseError(String),
-    Args(HashMap<String, Option<String>>),
-}
-
-pub fn parse_command_line_args() -> CmdLineParserResult {
-    let possible_args = HashMap::<&str, CmdLineArgValueKind>::from([
-        ("--in", CmdLineArgValueKind::Required),
-        ("--out", CmdLineArgValueKind::Required),
-        ("--dest-format", CmdLineArgValueKind::Required),
-        ("--help", CmdLineArgValueKind::NotRequired),
-    ]);
-
-    let mut collected_args: HashMap<String, Option<String>> =
-        HashMap::with_capacity(possible_args.len());
-
-    enum CmdLineParserState {
-        ReadArg,
-        ReadVal(String),
-    }
-
-    let mut args = CmdLineArgs::new();
-    args.next(); // skip program name
-
-    let mut result = CmdLineParserResult::NoArgs;
-    let mut parser_state = CmdLineParserState::ReadArg;
+    let mut args = env::args().skip(1).peekable(); // skip program name
     while let Some(arg) = args.next() {
-        match parser_state {
-            CmdLineParserState::ReadArg => {
-                if let Some((arg, val)) = arg.split_once('=') {
-                    args.push_back(val.to_string());
-                    args.push_back(arg.to_string());
-                } else {
-                    let value_kind = possible_args.get(&arg.as_str());
-                    if let Some(value_kind) = value_kind {
-                        match *value_kind {
-                            CmdLineArgValueKind::NotRequired => {
-                                collected_args.insert(arg, None);
-                            }
-                            CmdLineArgValueKind::Required => {
-                                parser_state = CmdLineParserState::ReadVal(arg)
-                            }
-                        }
+        match arg.as_str() {
+            "--in" | "--out" | "--dest-format" => {
+                let arg_needs_value = || format!("Argument {} needs value", arg);
+                if let Some(v) = args.next() {
+                    if !possible_args.contains(v.as_str()) {
+                        collected_args.insert(arg, Some(v));
                     } else {
-                        let err_msg = format!("Invalid argument: {}", arg);
-                        result = CmdLineParserResult::ParseError(err_msg);
-                        break;
+                        return Err(arg_needs_value());
                     }
-                }
+                } else {
+                    return Err(arg_needs_value());
+                };
             }
-            CmdLineParserState::ReadVal(ref arg_name) => {
-                if possible_args.contains_key(arg.as_str()) {
-                    break;
-                }
-
-                collected_args.insert(arg_name.clone(), Some(arg));
-                parser_state = CmdLineParserState::ReadArg;
+            "--help" => {
+                collected_args.insert(arg, None);
             }
-        }
-    }
-
-    if let CmdLineParserState::ReadVal(arg_name) = parser_state {
-        let err_msg = format!("Argument {} needs value", arg_name);
-        result = CmdLineParserResult::ParseError(err_msg)
-    }
-
-    if let CmdLineParserResult::NoArgs = result {
-        if !collected_args.is_empty() {
-            if collected_args.contains_key("--help") {
-                result = CmdLineParserResult::HelpSpecified;
-            } else if !collected_args.contains_key("--dest-format") {
-                result = CmdLineParserResult::ParseError(format!("--dest-format required"))
-            } else {
-                result = CmdLineParserResult::Args(collected_args);
+            _ => {
+                return Err(format!("Unknow argument: {}", arg));
             }
         }
     }
 
-    result
+    Ok(collected_args)
 }
 
 pub fn print_help() {
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
 // use std::time::{SystemTime, UNIX_EPOCH};
+
 mod cmd_line_parser;
-use cmd_line_parser::{CmdLineParserResult, parse_command_line_args, print_help};
+use cmd_line_parser::{parse_command_line_args, print_help};
 
 fn main() {
-    let cmd_parse_result = parse_command_line_args();
-    match cmd_parse_result {
-        CmdLineParserResult::NoArgs | CmdLineParserResult::HelpSpecified => print_help(),
-        CmdLineParserResult::ParseError(err_msg) => {
-            eprintln!("{}\nUse converter --help for more information.", err_msg);
+    const USE_MESSAGE: &str = "Use converter --help for more details.";
+    let result = parse_command_line_args();
+    match result {
+        Err(msg) => eprintln!("{}\n{}", msg, USE_MESSAGE),
+        Ok(args) => {
+            if args.is_empty() || args.contains_key("--help") {
+                print_help();
+            } else if !args.contains_key("--dest-format") {
+                eprintln!("--dest-format is required!\n{}", USE_MESSAGE)
+            } else {
+                ()
+            }
         }
-        CmdLineParserResult::Args(_) => (),
     }
 }
